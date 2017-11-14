1c0e5c4f98f8f43595173ff1316b1dbe8ac95cb7
Kazakov500 <kazakov.VADIM.2012@Yandex.ru>
2017-11-15 01:12:17 +0300

    All files

---
+++ annealing/Form1.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace annealing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tb_Count = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.cb_rand = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(634, 250);
+            this.panel1.TabIndex = 0;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(758, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tb_Count
+            // 
+            this.tb_Count.Location = new System.Drawing.Point(652, 28);
+            this.tb_Count.Name = "tb_Count";
+            this.tb_Count.Size = new System.Drawing.Size(100, 20);
+            this.tb_Count.TabIndex = 2;
+            this.tb_Count.Text = "100";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(758, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 55);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Optimaze";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(725, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(725, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // chart1
+            // 
+            chartArea3.AxisX.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chartArea3.AxisX.MinorGrid.Enabled = true;
+            chartArea3.AxisX.MinorGrid.LineColor = System.Drawing.Color.LightGray;
+            chartArea3.AxisY.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chartArea3.AxisY.MinorGrid.Enabled = true;
+            chartArea3.AxisY.MinorGrid.LineColor = System.Drawing.Color.LightGray;
+            chartArea3.Name = "ChartArea1";
+            chartArea3.Position.Auto = false;
+            chartArea3.Position.Height = 100F;
+            chartArea3.Position.Width = 99F;
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.Location = new System.Drawing.Point(12, 268);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.MarkerSize = 4;
+            series3.Name = "MainGraphic";
+            series3.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(821, 238);
+            this.chart1.TabIndex = 101;
+            this.chart1.Text = "chart2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(652, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 102;
+            this.textBox1.Text = "2000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(725, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 103;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(725, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 104;
+            this.label4.Text = "label4";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(652, 193);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 17);
+            this.checkBox1.TabIndex = 105;
+            this.checkBox1.Text = "show info";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(733, 239);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 106;
+            this.button3.Text = "Default";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(652, 106);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 107;
+            this.textBox2.Text = "200";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(652, 239);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 108;
+            this.button4.Text = "Optimaze2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(652, 216);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(86, 17);
+            this.checkBox2.TabIndex = 109;
+            this.checkBox2.Text = "Temperature";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // cb_rand
+            // 
+            this.cb_rand.AutoSize = true;
+            this.cb_rand.Location = new System.Drawing.Point(867, 67);
+            this.cb_rand.Name = "cb_rand";
+            this.cb_rand.Size = new System.Drawing.Size(47, 17);
+            this.cb_rand.TabIndex = 110;
+            this.cb_rand.Text = "rand";
+            this.cb_rand.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(867, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 111;
+            this.textBox3.Text = "5";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(867, 38);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 112;
+            this.button5.Text = "max dist";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(864, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 113;
+            this.label5.Text = "label5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(649, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 114;
+            this.label6.Text = "city count";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(652, 51);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 13);
+            this.label7.TabIndex = 115;
+            this.label7.Text = "iter count";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(651, 90);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 13);
+            this.label8.TabIndex = 116;
+            this.label8.Text = "iter2 count";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(652, 139);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(64, 13);
+            this.label9.TabIndex = 117;
+            this.label9.Text = "Start energy";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(652, 164);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(61, 13);
+            this.label10.TabIndex = 118;
+            this.label10.Text = "End energy";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(652, 177);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(67, 13);
+            this.label11.TabIndex = 119;
+            this.label11.Text = "Temperature";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 511);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.cb_rand);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tb_Count);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tb_Count;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox cb_rand;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ annealing/Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace annealing
+{
+    public partial class Form1 : Form
+    {
+        List<Point> cities = new List<Point>();
+        List<int> roads = new List<int>();
+
+        List<Point> def_cities;
+        List<int> def_roads;
+
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0));
+
+            foreach (var citie in cities)
+            {
+                e.Graphics.DrawEllipse(pen, citie.X, citie.Y, 3, 3);
+            }
+
+            //foreach (var road in roads)
+            for (int i = 0; i < roads.Count-1; i++)
+            {
+                e.Graphics.DrawLine(pen, cities[roads[i]].X, cities[roads[i]].Y, cities[roads[i + 1]].X, cities[roads[i+1]].Y);
+            }
+            if (cities.Count > 0)
+                e.Graphics.DrawLine(pen, cities[roads[0]].X, cities[roads[0]].Y, cities[roads[roads.Count - 1]].X, cities[roads[roads.Count - 1]].Y);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            roads.Clear();
+            cities.Clear();
+
+            int count = Convert.ToInt32(tb_Count.Text);
+            for (int i = 0; i < count; i++)
+            {
+                cities.Add(new Point(rnd.Next(1, panel1.Size.Width-1), rnd.Next(1, panel1.Size.Height-1)));
+            }
+
+            for (int i = 0; i < cities.Count; i++)
+            {
+                roads.Add(i);
+            }
+            //roads.Add(0);
+
+
+            label1.Text = CalculateEnergy(roads).ToString();
+
+            def_cities = new List<Point>(cities);
+            def_roads = new List<int>(roads);
+
+            panel1.Refresh();
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SimulatedAnnealing(10, 0.01f);//0.00001
+
+            panel1.Refresh();
+        }
+
+
+        void Reset_to_default_state()
+        {
+            cities = new List<Point>(def_cities);
+            roads = new List<int>(def_roads); 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Reset_to_default_state();
+
+            panel1.Refresh();
+        }
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            /*List<int> buff_roads = new List<int>(roads);
+            float buff_Energy;
+            float min_buff_Energy = CalculateEnergy(roads);
+
+            for (int i = 0; i < Convert.ToInt32(textBox2.Text); i++)
+            {
+                SimulatedAnnealing(10, 0.00001f);
+
+
+                buff_Energy = CalculateEnergy(roads);
+                if (buff_Energy < min_buff_Energy)
+                {
+                    buff_roads = new List<int>(roads);
+                    min_buff_Energy = buff_Energy;
+                }
+
+                Reset_to_default_state();
+            }*/
+            chart1.Series["MainGraphic"].Points.Clear();
+
+            for (int i = 0; i < Convert.ToInt32(textBox2.Text); i++)
+            {
+                this.Text = i.ToString();
+                button2.PerformClick();
+
+
+                chart1.Series["MainGraphic"].Points.AddXY(i, Convert.ToDouble(label3.Text));
+            }
+
+            
+
+            label5.Text = (Math.Round(((Convert.ToDouble(label1.Text) - Convert.ToDouble(label3.Text)) / Convert.ToDouble(label1.Text)), 3)).ToString() + "%";
+
+
+            //label2.Text = CalculateEnergy(roads).ToString();
+
+            panel1.Refresh();
+        }
+
+
+        #region Optimization
+
+        //===========Optimaze=========================================
+
+        float DecreaseTemperature(float initialTemperature, int i)
+        {
+            
+            
+
+            //initialTemperature - начальная температура
+            //i - номер итерации
+
+            return initialTemperature * 0.1f / (float)i;
+
+
+
+            //float D = roads.Count;
+            //float C = 1.1f;
+            //return initialTemperature * (float)Math.Exp(-C * Math.Pow(i, (1 / D)));
+        }
+
+        float GetTransitionProbability(float dEm, float T)
+        {
+            return (float)Math.Exp(-dEm/T);
+        }
+
+        bool IsTransition(float probability)
+        {
+            float value = rnd.Next(1000) / 1000f;//1000
+            return (value <= probability) ? true : false;
+        }
+
+        float distance(Point p1, Point p2)
+        {
+            return (float)Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+        }
+
+        List<int> GenerateStateCandidate(List<int> state)
+        {
+            //int count = rnd.Next(0, state.Count/2);
+            List<int> buf = new List<int>(state);
+
+
+            int c1 = rnd.Next(roads.Count);
+            int c2 = rnd.Next(roads.Count);
+
+
+            if (c1 < c2)
+                buf.Reverse(c1, c2 - c1 + 1);
+            else
+                buf.Reverse(c2, c1 - c2 + 1);
+
+            return buf;
+        }
+
+        float CalculateEnergy(List<int> state)
+        {
+            int n = state.Count;
+            float E = 0;
+
+            for (int i = 0; i < state.Count-1; i++)
+            {
+                E = E + distance(cities[state[i]], cities[state[i+1]]);
+            }
+            E = E + distance(cities[state[0]], cities[state[state.Count - 1]]);
+
+            return E;
+
+        }
+
+
+        List<int> min_roads;
+        float min_energy;
+
+        void SimulatedAnnealing(float initialTemperature, float endTemperature)
+        {
+            chart1.Series[0].Points.Clear();
+
+
+            int n = cities.Count;
+
+            float currentEnergy = CalculateEnergy(roads);
+            min_energy = currentEnergy;
+            min_roads = new List<int>(roads);
+            label2.Text = currentEnergy.ToString();
+
+            float T = initialTemperature;
+    
+            int i = 0;
+            while ((i < Convert.ToInt32(textBox1.Text)) || (checkBox2.Checked && (T >= endTemperature)))
+            {
+                i++;
+                List<int> stateCandidate = GenerateStateCandidate(roads);
+                float candidateEnergy = CalculateEnergy(stateCandidate);
+        
+                if(candidateEnergy < currentEnergy)
+                {
+                    currentEnergy = candidateEnergy;
+                    roads = stateCandidate;
+                }
+                else
+                {
+                    float p = GetTransitionProbability(candidateEnergy-currentEnergy, T);
+                    if (IsTransition(p))
+                    {
+                        currentEnergy = candidateEnergy;
+                        roads = stateCandidate;
+                    }
+                }
+
+                T = DecreaseTemperature(initialTemperature, i);
+
+                chart1.Series["MainGraphic"].Points.AddXY(i, currentEnergy);
+                chart1.Invalidate();
+
+                
+                if (min_energy > currentEnergy)
+                {
+
+                    min_energy = currentEnergy;
+                    min_roads = new List<int>(roads);
+                }
+
+                if (checkBox1.Checked)
+                {
+                    label3.Text = CalculateEnergy(roads).ToString();
+                    label3.Refresh();
+                    label4.Text = ((decimal)T).ToString();
+                    label4.Refresh();
+                }
+
+            }
+
+
+            roads = new List<int>(min_roads);
+
+            label3.Text = CalculateEnergy(roads).ToString();
+
+
+            //panel1.Refresh();
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            float max_dist = float.MinValue;
+            int c1 = 0;
+            int c2 = 0;
+            float dist;
+            int q1 = 0;
+            int q2 = 0;
+
+
+            for (int i = 0; i < roads.Count-1; i++)
+			{
+			 
+                dist = distance(cities[roads[i]], cities[roads[i+1]]);
+                if (max_dist < dist)
+                {
+                    max_dist = dist;
+                    c1 = roads[i];
+                    c2 = roads[i + 1];
+                    q1 = i;
+                    q2 = i + 1;
+                }
+            }
+            dist = distance(cities[roads[0]], cities[roads[roads.Count - 1]]);
+            if (max_dist < dist)
+            {
+                max_dist = dist;
+                q1 = 0;
+                q2 = roads.Count - 1;
+                c1 = roads[0];
+                c2 = roads[roads.Count - 1];
+            }
+
+
+            MessageBox.Show(q1.ToString() + " " + q2.ToString() + " " + c1.ToString() + " " + c2.ToString() + " " + max_dist.ToString());
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            label5.Text = (Math.Round(((Convert.ToDouble(label1.Text) - Convert.ToDouble(label3.Text)) / Convert.ToDouble(label1.Text)), 3)).ToString() + "%";
+        }
+
+
+
+        //===========Optimaze=========================================
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.PerformClick();
+        }
+    }
+}
